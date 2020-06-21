c994fc8e7acf3e3524dc5315b875aa54d6a1cc20
wayn <1669738430@qq.com>
2020-06-21 23:45:11 +0800

    feat(后台管理): 添加banner管理

--- waynboot-admin-api/src/main/java/com/wayn/admin/api/domain/shop/ShopBanner.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/domain/shop/ShopBanner.java
@@ -21,13 +21,14 @@ public class ShopBanner extends BaseEntity implements Serializable {
     @NotBlank(message = "标题不能为空")
     private String title;
 
+    @NotBlank(message = "banner图片不能为空")
     private String imgUrl;
-
-    private String page;
 
     private String jumpUrl;
 
     private Integer sort;
+
+    private Integer status;
 
     /**
      * 删除标志（0代表存在 2代表删除）
