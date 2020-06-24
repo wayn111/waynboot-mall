3be3ae2730f2702408838f96d53adaa651eca6e3
wayn <1669738430@qq.com>
2020-06-24 23:48:28 +0800

    feat(后台管理): 完善代码

--- waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/BannerController.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/BannerController.java
@@ -47,6 +47,7 @@ public class BannerController extends BaseController {
 
     @DeleteMapping("{bannerId}")
     public R deleteChannel(@PathVariable Long bannerId) {
-        return R.result(iBannerService.removeById(bannerId));
+        return R.result(iBannerService.removeById(bannerId)
+        );
     }
 }
