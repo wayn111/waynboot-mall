4248265e345d0e20878f8e3da031e99efcd40912
waynaqua <1669738430@qq.com>
2020-07-23 16:28:35 +0800

    feat(后台管理): 会员模块 会员列表开发完成

---
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/service/shop/IMemberService.java
@@ -0,0 +1,28 @@
+package com.wayn.admin.api.service.shop;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wayn.admin.api.domain.shop.Banner;
+import com.wayn.admin.api.domain.shop.Member;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户表 服务类
+ * </p>
+ *
+ * @author wayn
+ * @since 2020-07-21
+ */
+public interface IMemberService extends IService<Member> {
+
+    /**
+     * 查询会员分页列表
+     *
+     * @param page   分页对象
+     * @param member 查询参数
+     * @return 会员分页列表
+     */
+    IPage<Member> listPage(Page<Member> page, Member member);
+
+}
