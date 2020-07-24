ec863e3e14b152279c57ab222c88b7b2f72dbf3a
waynaqua <1669738430@qq.com>
2020-07-24 17:08:40 +0800

    fix(后台管理): 栏目管理 修复bug

--- waynboot-admin-api/src/main/java/com/wayn/AdminApplication.java
+++ waynboot-admin-api/src/main/java/com/wayn/AdminApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WaynbootApplication {
+public class AdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WaynbootApplication.class, args);
+        SpringApplication.run(AdminApplication.class, args);
     }
 }
