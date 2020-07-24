405a8750cb83fbd68c67dd804b47fcb641057eb4
waynaqua <1669738430@qq.com>
2020-07-24 18:28:59 +0800

    feat(前台): Api项目 初始化

---
+++ waynboot-mobile/src/main/java/com/wayn/AdminApplication.java
@@ -0,0 +1,12 @@
+package com.wayn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileApplication.class, args);
+    }
+}
---
+++ waynboot-mobile/src/main/java/com/wayn/mobile/api/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.wayn.mobile.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "hello world!";
+    }
+}
