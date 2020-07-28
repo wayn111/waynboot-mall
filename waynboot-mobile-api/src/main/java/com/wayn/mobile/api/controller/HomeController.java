77d5559db818a3f93f641706e789ed4515604ead
waynaqua <1669738430@qq.com>
2020-07-28 19:18:05 +0800

    feat(前台): 添加首页接口

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/HomeController.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/HomeController.java
@@ -1,11 +1,17 @@
 package com.wayn.mobile.api.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wayn.common.base.BaseController;
 import com.wayn.common.core.domain.shop.Banner;
 import com.wayn.common.core.domain.shop.Category;
+import com.wayn.common.core.domain.shop.Goods;
+import com.wayn.common.core.domain.system.Dict;
 import com.wayn.common.core.service.shop.IBannerService;
 import com.wayn.common.core.service.shop.ICategoryService;
+import com.wayn.common.core.service.shop.IGoodsService;
 import com.wayn.common.util.R;
+import com.wayn.mobile.api.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,35 +22,20 @@ import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("home")
-public class HomeController {
+public class HomeController extends BaseController {
 
     @Autowired
-    private IBannerService iBannerService;
-
-    @Autowired
-    private ICategoryService iCategoryService;
+    private HomeService homeService;
 
     @PostMapping("index")
-    public R getInfo() {
-        R success = R.success();
-        ThreadPoolExecutor poolExecutor = new ThreadPoolExecutor(10, 10,
-                0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(), r -> new Thread(r,"首页线程"));
-        Callable<List<Banner>> bannerCall = () -> iBannerService.list(new QueryWrapper<Banner>().eq("status", 0).orderByAsc("sort_order"));
-        Callable<List<Category>> categoryCall = () -> iCategoryService.list(new QueryWrapper<Category>().eq("level", "L1").orderByAsc("sort_order"));
-        FutureTask<List<Banner>> bannerTask = new FutureTask<>(bannerCall);
-        poolExecutor.submit(bannerTask);
-        FutureTask<List<Category>> categoryTask = new FutureTask<>(categoryCall);
-        poolExecutor.submit(categoryTask);
-        try {
-            success.add("bannerList", bannerTask.get());
-            success.add("categoryList", categoryTask.get());
-        } catch (InterruptedException | ExecutionException e) {
-            e.printStackTrace();
-        } finally {
-            poolExecutor.shutdown();
-        }
-        return success;
+    public R getHomeIndex() {
+        return homeService.getHomeIndexData();
+    }
+
+    @PostMapping("goodsList")
+    public R getGoodsList() {
+        Page<Goods> page = getPage();
+        return homeService.listGoodsPage(page);
     }
 }
 
---
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/HomeService.java
@@ -0,0 +1,18 @@
+package com.wayn.mobile.api.service;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.wayn.common.core.domain.shop.Goods;
+import com.wayn.common.util.R;
+
+public interface HomeService {
+
+    /**
+     * 获取首月数据（bannerList，category List，newGoodsList，hotGoodsList）
+     * @return
+     */
+    R getHomeIndexData();
+
+    R getGoodsList();
+
+    R listGoodsPage(Page<Goods> page);
+}
