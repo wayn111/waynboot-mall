1582986de1b2fe46f71b1d2d3f7a9d0d5dc3afd6
wayn <1669738430@qq.com>
2020-07-28 23:02:40 +0800

    perf(商城): 优化商城pom结构

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/HomeController.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/HomeController.java
@@ -1,24 +1,15 @@
 package com.wayn.mobile.api.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wayn.common.base.BaseController;
-import com.wayn.common.core.domain.shop.Banner;
-import com.wayn.common.core.domain.shop.Category;
 import com.wayn.common.core.domain.shop.Goods;
-import com.wayn.common.core.domain.system.Dict;
-import com.wayn.common.core.service.shop.IBannerService;
-import com.wayn.common.core.service.shop.ICategoryService;
-import com.wayn.common.core.service.shop.IGoodsService;
 import com.wayn.common.util.R;
 import com.wayn.mobile.api.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.concurrent.*;
 
 @RestController
 @RequestMapping("home")
@@ -32,7 +23,7 @@ public class HomeController extends BaseController {
         return homeService.getHomeIndexData();
     }
 
-    @PostMapping("goodsList")
+    @GetMapping("goodsList")
     public R getGoodsList() {
         Page<Goods> page = getPage();
         return homeService.listGoodsPage(page);
