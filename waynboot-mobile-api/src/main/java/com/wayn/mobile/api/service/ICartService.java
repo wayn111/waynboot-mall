9fc8060daafa1b6cfd675591daf9bdfb6352e027
waynaqua <1669738430@qq.com>
2020-08-05 11:44:40 +0800

    feat(手机端): 购物车模块 完善购物车模块

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/ICartService.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/ICartService.java
@@ -1,5 +1,6 @@
 package com.wayn.mobile.api.service;
 
+import com.wayn.common.util.R;
 import com.wayn.mobile.api.domain.Cart;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -20,5 +21,22 @@ public interface ICartService extends IService<Cart> {
      * @param productId
      * @return
      */
-    boolean checkExistsGoods(Long userId, Integer goodsId, Integer productId);
+    Cart checkExistsGoods(Long userId, Integer goodsId, Integer productId);
+
+    /**
+     * 加入商品到购物车
+     * <p>
+     * 如果已经存在购物车货品，则增加数量；
+     * 否则添加新的购物车货品项。
+     *
+     * @param cart 购物车商品信息， { goodsId: xxx, productId: xxx, number: xxx }
+     * @return R
+     */
+    R addCart(Cart cart);
+
+    /**
+     * 计算购物车中商品数量
+     * @return R
+     */
+    R goodsCount();
 }
