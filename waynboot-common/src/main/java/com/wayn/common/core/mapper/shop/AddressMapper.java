bc81fed672af3d4dd645255a44147d4e1b1bb360
hequan_waynaqua <1669738430@qq.com>
2020-08-07 15:54:54 +0800

    feat(手机端): 用户地址 添加用户地址列表接口

---
+++ waynboot-common/src/main/java/com/wayn/common/core/mapper/shop/AddressMapper.java
@@ -0,0 +1,8 @@
+package com.wayn.common.core.mapper.shop;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wayn.common.core.domain.shop.Address;
+
+public interface AddressMapper extends BaseMapper<Address> {
+
+}
---
+++ waynboot-common/src/main/java/com/wayn/common/core/service/shop/IAddressService.java
@@ -0,0 +1,8 @@
+package com.wayn.common.core.service.shop;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wayn.common.core.domain.shop.Address;
+
+public interface IAddressService extends IService<Address> {
+
+}
