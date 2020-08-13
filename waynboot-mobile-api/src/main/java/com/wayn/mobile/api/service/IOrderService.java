74770074eb674fe229987e9983fdbef650f17efc
hequan_waynaqua <1669738430@qq.com>
2020-08-13 16:20:48 +0800

    feat(手机端): 订单模块 添加订单支付接口

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/IOrderService.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/IOrderService.java
@@ -1,9 +1,11 @@
 package com.wayn.mobile.api.service;
 
+import com.baomidou.mybatisplus.extension.service.IService;
 import com.wayn.common.util.R;
 import com.wayn.mobile.api.domain.Order;
-import com.baomidou.mybatisplus.extension.service.IService;
 import com.wayn.mobile.api.domain.vo.OrderVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
@@ -17,9 +19,14 @@ public interface IOrderService extends IService<Order> {
 
     /**
      * 添加订单记录
+     *
      * @param orderVO 订单VO
      * @return R
      */
     R addOrder(OrderVO orderVO);
+
+    R h5pay(Long orderId, HttpServletRequest request);
+
+    R prepay(Long orderId, HttpServletRequest request);
 
 }
