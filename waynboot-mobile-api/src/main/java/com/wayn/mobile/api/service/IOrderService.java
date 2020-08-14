bdbf3919c4940764c640b0605aef8b432a5bca0d
hequan_waynaqua <1669738430@qq.com>
2020-08-14 19:15:46 +0800

    feat(手机端): 订单模块 完善订单支付接口

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/IOrderService.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/service/IOrderService.java
@@ -25,8 +25,25 @@ public interface IOrderService extends IService<Order> {
      */
     R addOrder(OrderVO orderVO);
 
+    /**
+     * 微信H5支付
+     * @param orderId 订单ID
+     * @param request 请求
+     * @return r
+     */
     R h5pay(Long orderId, HttpServletRequest request);
 
+    /**
+     * 付款订单的预支付会话标识
+     * <p>
+     * 1. 检测当前订单是否能够付款
+     * 2. 微信商户平台返回支付订单ID
+     * 3. 设置订单付款状态
+     *
+     * @param orderId 订单ID
+     * @param request 请求
+     * @return r
+     */
     R prepay(Long orderId, HttpServletRequest request);
 
 }
