8550c3393b7cb261c1a899e9a99c562ab939e7ad
wayn <1669738430@qq.com>
2020-09-04 23:37:27 +0800

    refactor(商城): 订单管理 优化订单发货操作

--- waynboot-common/src/main/java/com/wayn/common/core/domain/vo/ShipVO.java
+++ waynboot-common/src/main/java/com/wayn/common/core/domain/vo/ShipVO.java
@@ -2,6 +2,9 @@ package com.wayn.common.core.domain.vo;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * 订单发货VO
  */
@@ -11,15 +14,18 @@ public class ShipVO {
     /**
      * 订单ID
      */
+    @NotNull(message = "订单ID不能为空")
     private Long orderId;
 
     /**
      * 发货渠道
       */
+    @NotBlank(message = "发货渠道不能为空")
     private String shipChannel;
 
     /**
      * 发货编号
      */
+    @NotBlank(message = "发货编号不能为空")
     private String shipSn;
 }
