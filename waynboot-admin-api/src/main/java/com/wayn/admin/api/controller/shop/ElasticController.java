95e15ee4ef0d083b4c0eed2f6338f4be16bd4cd3
wayn <1669738430@qq.com>
2020-10-11 23:07:39 +0800

    feat(商城): 添加elasticsearch

--- waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/ElasticController.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/ElasticController.java
@@ -3,15 +3,16 @@ package com.wayn.admin.api.controller.shop;
 
 import com.wayn.admin.framework.manager.elastic.service.BaseElasticService;
 import com.wayn.common.base.ElasticEntity;
+import com.wayn.common.core.domain.shop.Goods;
+import com.wayn.common.core.service.shop.IGoodsService;
 import com.wayn.common.util.R;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,24 +24,32 @@ public class ElasticController {
     @Autowired
     private BaseElasticService baseElasticService;
 
+    @Autowired
+    private IGoodsService iGoodsService;
+
     @GetMapping("insert")
     public R insert() {
-        ElasticEntity elasticEntity = new ElasticEntity();
-        elasticEntity.setId("2");
-        Map<String, Object> map = new HashMap<>();
-        map.put("name", "书本2");
-        map.put("price", 1000D);
-        map.put("date", new Date());
-        elasticEntity.setData(map);
-        baseElasticService.insertOrUpdateOne("store", elasticEntity);
+        List<Goods> list = iGoodsService.list();
+        List<ElasticEntity> entities = new ArrayList<>();
+        for (Goods goods : list) {
+            ElasticEntity elasticEntity = new ElasticEntity();
+            Map<String, Object> map = new HashMap<>();
+            elasticEntity.setId(goods.getId().toString());
+            map.put("name", goods.getName());
+            map.put("keyword", goods.getKeywords());
+            map.put("isOnSale", goods.getIsOnSale());
+            elasticEntity.setData(map);
+            entities.add(elasticEntity);
+        }
+        baseElasticService.insertBatch("goods", entities);
         return R.success();
     }
 
     @GetMapping("search")
     public R search() {
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "沃尔玛"));
-        List<Object> list = baseElasticService.search("store", searchSourceBuilder, Object.class);
+//        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "沃尔玛"));
+        List<Object> list = baseElasticService.search("goods", searchSourceBuilder, Object.class);
         return R.success().add("data", list);
     }
 }
--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/SearchController.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/SearchController.java
@@ -1,6 +1,7 @@
 package com.wayn.mobile.api.controller;
 
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wayn.common.base.BaseController;
 import com.wayn.common.core.domain.shop.Goods;
@@ -9,8 +10,11 @@ import com.wayn.common.core.service.shop.IGoodsService;
 import com.wayn.common.util.R;
 import com.wayn.mobile.api.domain.SearchHistory;
 import com.wayn.mobile.api.service.ISearchHistoryService;
+import com.wayn.mobile.framework.manager.service.BaseElasticService;
 import com.wayn.mobile.framework.security.util.SecurityUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,13 +41,16 @@ public class SearchController extends BaseController {
     @Autowired
     private ISearchHistoryService iSearchHistoryService;
 
+    @Autowired
+    private BaseElasticService baseElasticService;
+
     @GetMapping("result")
     public R list(SearchVO searchVO) {
         Long memberId = SecurityUtils.getUserId();
         String keyword = searchVO.getKeyword();
-        Integer categoryId = searchVO.getCategoryId();
-        Boolean isHot = searchVO.getIsHot();
-        Boolean isNew = searchVO.getIsNew();
+//        Integer categoryId = searchVO.getCategoryId();
+//        Boolean isHot = searchVO.getIsHot();
+//        Boolean isNew = searchVO.getIsNew();
         if (memberId != null && StringUtils.isNotEmpty(keyword)) {
             SearchHistory searchHistory = new SearchHistory();
             searchHistory.setCreateTime(LocalDateTime.now());
@@ -51,6 +58,18 @@ public class SearchController extends BaseController {
             searchHistory.setKeyword(keyword);
             iSearchHistoryService.save(searchHistory);
         }
+        // matchQuery
+        MatchQueryBuilder matchQuery = QueryBuilders.matchQuery(keyword, "name").analyzer("standard");
+        // TermQuery
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("keyword", keyword);
+        // 布尔查询
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.should(matchQuery);
+        boolQueryBuilder.should(termQueryBuilder);
+        // 设置布尔查询对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(boolQueryBuilder);
+        List<JSONObject> objectList = baseElasticService.search("goods", searchSourceBuilder, JSONObject.class);
         Page<SearchVO> page = getPage();
         List<Goods> goods = iGoodsService.searchResult(page, searchVO);
         return R.success().add("goods", goods);
