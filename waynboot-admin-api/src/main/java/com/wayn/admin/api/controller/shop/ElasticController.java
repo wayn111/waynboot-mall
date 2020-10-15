dab74e43f52325aa7cdaf056a6f437b242166571
wayn <1669738430@qq.com>
2020-10-15 21:04:26 +0800

    feat(商城): 提交测试文件

--- waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/ElasticController.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/shop/ElasticController.java
@@ -86,6 +86,7 @@ public class ElasticController {
 //        request.add(firstSearchRequest);
 //        request.add(secondSearchRequest);
 //        List<Object> list = baseElasticService.search(request, Object.class);
+        // 查询
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         MatchQueryBuilder matchQuery = QueryBuilders.matchQuery("name", "手机");
