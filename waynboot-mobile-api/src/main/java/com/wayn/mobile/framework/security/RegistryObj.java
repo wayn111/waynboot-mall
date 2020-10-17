fcca042b8b8c05fe22aa69177e3d36710d7bffad
hequan_waynaqua <1669738430@qq.com>
2020-10-17 17:02:17 +0800

    feat(手机端): 购物车模块 添加购物车模块基础代码

---
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/framework/security/RegistryObj.java
@@ -0,0 +1,43 @@
+package com.wayn.mobile.framework.security;
+
+import lombok.Data;
+
+@Data
+public class RegistryObj {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 重复密码
+     */
+    private String confirmPassword;
+
+    /**
+     * 验证码
+     */
+    private String code;
+
+    /**
+     * 验证码 key
+     */
+    private String key;
+
+}
