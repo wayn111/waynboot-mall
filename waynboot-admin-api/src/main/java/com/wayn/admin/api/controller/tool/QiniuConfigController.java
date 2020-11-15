43f0ac0b36c41442bcff3804b67bc927e791de54
wayn <1669738430@qq.com>
2020-11-15 12:54:32 +0800

    feat(后台管理): 七牛云管理
    添加七牛云管理基础代码

--- waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/tool/QiniuConfigController.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/api/controller/tool/QiniuConfigController.java
@@ -1,13 +1,19 @@
 package com.wayn.admin.api.controller.tool;
 
 
+import com.wayn.admin.framework.config.WaynConfig;
+import com.wayn.admin.framework.manager.upload.service.UploadService;
 import com.wayn.common.core.domain.tool.QiniuConfig;
 import com.wayn.common.core.service.tool.IQiniuConfigService;
 import com.wayn.common.util.R;
+import com.wayn.common.util.file.FileUploadUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("tool/qiniu")
@@ -15,6 +21,9 @@ public class QiniuConfigController {
 
     @Autowired
     private IQiniuConfigService iQiniuConfigService;
+
+    @Autowired
+    private UploadService uploadService;
 
     @GetMapping("config")
     public R info() {
@@ -26,5 +35,21 @@ public class QiniuConfigController {
         qiniuConfig.setId(1L);
         iQiniuConfigService.updateById(qiniuConfig);
         return R.success("修改成功");
+    }
+
+    @PostMapping
+    public R upload(@RequestParam MultipartFile file) throws IOException {
+        QiniuConfig qiniuConfig = iQiniuConfigService.getById(1);
+        if (qiniuConfig == null) {
+            return R.error("七牛云配置不存");
+        }
+        if (StringUtils.isEmpty(qiniuConfig.getAccessKey())) {
+            return R.error("七牛云配置错误");
+        }
+        // 上传文件路径
+        String filePath = WaynConfig.getUploadDir();
+        String fileName = FileUploadUtil.uploadFile(file, filePath);
+        String fileUrl = uploadService.qiniuUploadFile(fileName);
+        return R.success().add("fileUrl", fileUrl);
     }
 }
