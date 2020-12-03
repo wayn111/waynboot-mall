e0aa640a96a42791e106a60d34c1a9d495276e14
wayn <1669738430@qq.com>
2020-12-03 22:46:25 +0800

    feat(后台管理): 运营管理
    添加栏目菜单列表

--- waynboot-common/src/main/java/com/wayn/common/core/mapper/shop/ColumnMapper.java
+++ waynboot-common/src/main/java/com/wayn/common/core/mapper/shop/ColumnMapper.java
@@ -1,6 +1,8 @@
 package com.wayn.common.core.mapper.shop;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.wayn.common.core.domain.shop.Column;
 
 /**
@@ -13,4 +15,5 @@ import com.wayn.common.core.domain.shop.Column;
  */
 public interface ColumnMapper extends BaseMapper<Column> {
 
+    IPage<Column> selectColumnListPage(Page<Column> page, Column column);
 }
