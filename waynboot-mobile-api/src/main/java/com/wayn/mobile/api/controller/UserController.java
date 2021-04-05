a2bcb47a229cc57855b399f5fee689949081a1e7
wayn <1669738430@qq.com>
2021-04-05 11:03:51 +0800

    refactor(移动端): 代码优化

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/UserController.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.wayn.mobile.api.controller;
 
 import com.wayn.common.core.domain.shop.Member;
+import com.wayn.common.core.domain.vo.ProfileVO;
 import com.wayn.common.core.service.shop.IMemberService;
 import com.wayn.common.exception.BusinessException;
 import com.wayn.common.util.R;
 import com.wayn.common.util.ServletUtils;
 import com.wayn.mobile.framework.security.LoginUserDetail;
 import com.wayn.mobile.framework.security.service.TokenService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("user")
@@ -27,6 +29,35 @@ public class UserController {
     public R getInfo() {
         LoginUserDetail loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
         return R.success().add("info", loginUser.getMember());
+    }
+
+    @PostMapping("profile")
+    public R profile(@RequestBody ProfileVO profileVO) {
+        String nickname = profileVO.getNickname();
+        Integer gender = profileVO.getGender();
+        String mobile = profileVO.getMobile();
+        String email = profileVO.getEmail();
+        LocalDate birthday = profileVO.getBirthday();
+        LoginUserDetail loginUser = tokenService.getLoginUser(ServletUtils.getRequest());
+        Member member = loginUser.getMember();
+        if (StringUtils.isNotBlank(nickname)) {
+            member.setNickname(nickname);
+        }
+        if (Objects.nonNull(gender)) {
+            member.setGender(gender);
+        }
+        if (StringUtils.isNotBlank(mobile)) {
+            member.setMobile(mobile);
+        }
+        if (StringUtils.isNotBlank(email)) {
+            member.setEmail(email);
+        }
+        if (Objects.nonNull(birthday)) {
+            member.setBirthday(birthday);
+        }
+        loginUser.setMember(member);
+        tokenService.refreshToken(loginUser);
+        return R.result(iMemberService.updateById(member));
     }
 
     @PostMapping("uploadAvatar")
