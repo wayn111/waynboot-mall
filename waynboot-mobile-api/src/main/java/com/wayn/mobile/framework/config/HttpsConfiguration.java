7bd554a36793fe3fa8eb2c8b97ccbc01aab3cbfd
hequan_waynaqua <1669738430@qq.com>
2021-04-19 15:28:58 +0800

    perf(商城): 添加https支持

---
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/framework/config/HttpsConfiguration.java
@@ -0,0 +1,48 @@
+package com.wayn.mobile.framework.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpsConfiguration {
+
+    @Value("${http-port}")
+    private int port;
+
+    @Value("${server.port}")
+    private int sslPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(port);
+        connector.setSecure(false);
+        connector.setRedirectPort(sslPort);
+        return connector;
+    }
+
+}
