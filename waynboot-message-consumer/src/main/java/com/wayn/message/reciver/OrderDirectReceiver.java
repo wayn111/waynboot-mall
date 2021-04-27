59944c46ed92e1911f0bc44f4fbff112d5ea52b3
wayn <1669738430@qq.com>
2021-04-28 00:54:08 +0800

    feat(移动端): rabbitmq添加生产者消息确认和消费者确认

--- waynboot-message-consumer/src/main/java/com/wayn/message/reciver/OrderDirectReceiver.java
+++ waynboot-message-consumer/src/main/java/com/wayn/message/reciver/OrderDirectReceiver.java
@@ -1,8 +1,12 @@
 package com.wayn.message.reciver;
 
+
+import com.alibaba.fastjson.JSONObject;
+import com.rabbitmq.client.Channel;
+import com.wayn.message.redis.RedisCache;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -14,36 +18,53 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
+import java.io.IOException;
 
 @Slf4j
 @Component
-@RabbitListener(queues = "OrderDirectQueue")
 public class OrderDirectReceiver {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    @RabbitHandler
-    public void process(Map<String, Object> testMessage) {
-        log.info("OrderDirectReceiver消费者收到消息: {}", testMessage.toString());
-        String notifyUrl = (String) testMessage.get("notifyUrl");
-        if (StringUtils.isEmpty(notifyUrl)) {
-            log.error("notifyUrl不能为空！，参数：{}", testMessage);
+    @Autowired
+    private RedisCache redisCache;
+
+    @RabbitListener(queues = "OrderDirectQueue")
+    public void process(Channel channel, Message message) throws IOException {
+        String body = new String(message.getBody());
+        log.info("OrderDirectReceiver消费者收到消息: {}", body);
+        String msgId = message.getMessageProperties().getCorrelationId();
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        // 消费者消费消息时幂等性处理
+        if (redisCache.getCacheSet("order_consumer_set").contains(msgId)) {
+            // redis中包含该 key，说明该消息已经被消费过
+            log.info(msgId + ":消息已经被消费");
+            channel.basicAck(deliveryTag, false);// 确认消息已消费
             return;
         }
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
-        multiValueMap.add("order", testMessage.get("order"));
-        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(multiValueMap, headers);
+
         try {
+            JSONObject msgObject = JSONObject.parseObject(body);
+            String notifyUrl = (String) msgObject.get("notifyUrl");
+            if (StringUtils.isEmpty(notifyUrl)) {
+                throw new Exception("获取notifyUrl失败");
+            }
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+            MultiValueMap<String, Object> multiValueMap = new LinkedMultiValueMap<>();
+            multiValueMap.add("order", msgObject.get("order"));
+            HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<>(multiValueMap, headers);
             ResponseEntity<String> response = restTemplate.postForEntity(notifyUrl, request, String.class);
             if (response.getStatusCode().value() != 200) {
-                throw new Exception(testMessage + " 下单失败");
+                throw new Exception("调用订单系统下单失败 ：" + msgObject);
             }
+            // 确认收到消息，false只确认当前consumer一个消息收到，true确认所有consumer获得的消息
+            channel.basicAck(deliveryTag, false);
         } catch (Exception e) {
+            channel.basicNack(deliveryTag, false, true);
             log.error(e.getMessage(), e);
         }
+
     }
 }
---
+++ waynboot-message-core/src/main/java/com/wayn/message/core/config/RabbitTemplateConfig.java
@@ -0,0 +1,36 @@
+package com.wayn.message.core.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * RabbitTemplate配置，设置生产者confirm确认和消费者return确认
+ */
+@Component
+public class RabbitTemplateConfig {
+
+    @Bean
+    public RabbitTemplate createRabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate();
+        rabbitTemplate.setConnectionFactory(connectionFactory);
+        // 设置开启Mandatory,才能触发回调函数,无论消息推送结果怎么样都强制调用回调函数
+        rabbitTemplate.setMandatory(true);
+
+        rabbitTemplate.setConfirmCallback((correlationData, ack, cause) -> {
+            System.out.println("ConfirmCallback:     " + "相关数据：" + correlationData);
+            System.out.println("ConfirmCallback:     " + "确认情况：" + ack);
+            System.out.println("ConfirmCallback:     " + "原因：" + cause);
+        });
+        rabbitTemplate.setReturnsCallback(returned -> {
+            System.out.println("ReturnCallback:     " + "消息：" + returned.getMessage());
+            System.out.println("ReturnCallback:     " + "回应码：" + returned.getReplyCode());
+            System.out.println("ReturnCallback:     " + "回应信息：" + returned.getReplyText());
+            System.out.println("ReturnCallback:     " + "交换机：" + returned.getExchange());
+            System.out.println("ReturnCallback:     " + "路由键：" + returned.getRoutingKey());
+        });
+
+        return rabbitTemplate;
+    }
+}
