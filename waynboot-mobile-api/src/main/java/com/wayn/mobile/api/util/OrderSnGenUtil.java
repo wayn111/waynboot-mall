4b50fe5b7048057b1cdd81ffeed877358ab711b6
waynaqua <1669738430@qq.com>
2021-06-27 18:33:26 +0800

    docs(商城): 代码优化
    1. 更新readme

--- waynboot-mobile-api/src/main/java/com/wayn/mobile/api/util/OrderSnGenUtil.java
+++ waynboot-mobile-api/src/main/java/com/wayn/mobile/api/util/OrderSnGenUtil.java
@@ -23,7 +23,7 @@ public class OrderSnGenUtil {
      */
     public static String generateOrderSn(Long userId) {
         long now = LocalDateTime.now().toEpochSecond(ZoneOffset.of("+8"));
-        return now + encryptUserId(userId.toString()) + countByOrderSn(userId);
+        return now + encryptUserId(String.valueOf(userId), 6) + countByOrderSn(userId);
     }
 
     /**
@@ -34,16 +34,19 @@ public class OrderSnGenUtil {
      */
     public static int countByOrderSn(Long userId) {
         IOrderService orderService = SpringContextUtil.getBean(IOrderService.class);
-        return orderService.count(new QueryWrapper<Order>().eq("user_id", userId).gt("create_time", LocalDate.now()).lt("create_time", LocalDate.now().plusDays(1)));
+        return orderService.count(new QueryWrapper<Order>().eq("user_id", userId)
+                .gt("create_time", LocalDate.now())
+                .lt("create_time", LocalDate.now().plusDays(1)));
     }
 
     /**
      * 加密用户ID，返回num位字符串
      *
      * @param userId 用户ID
+     * @param num    长度
      * @return num位加密字符串
      */
-    private static String encryptUserId(String userId) {
-        return String.format("%0" + 6 + "d", Integer.parseInt(userId) + 1);
+    private static String encryptUserId(String userId, int num) {
+        return String.format("%0" + num + "d", Integer.parseInt(userId) + 1);
     }
 }
