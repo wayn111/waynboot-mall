a197cb58fc26f65c74c88cc892c94b5638399ec1
wayn <1669738430@qq.com>
2021-07-10 19:45:58 +0800

    refactor(商城): 技术架构
    docker部署优化

--- waynboot-admin-api/run.sh
+++ waynboot-admin-api/run.sh
@@ -8,21 +8,23 @@ app_port=81
 app_version='1.1.0'
 # 定义应用环境
 profile_active='dev'
+# coding docker host
+coding_docker_host="waynaqua-docker.pkg.coding.net/wayn"
 echo '----copy jar----'
 docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker rmi ${group_name}/${app_name}:${app_version}
+docker rmi ${coding_docker_host}/${group_name}/${app_name}:${app_version}
 echo '----rm image----'
-# 打包编译docker镜像
-docker build -t ${group_name}/${app_name}:${app_version} .
-echo '----build image----'
+# 获取docker镜像
+docker pull ${coding_docker_host}/${group_name}/${app_name}:${app_version}
+echo '----pull image----'
 docker run -p ${app_port}:${app_port} --name ${app_name} \
---link mysql:db \
--e 'spring.profiles.active'=${profile_active} \
--e TZ="Asia/Shanghai" \
--v /etc/localtime:/etc/localtime \
--v /mydata/app/${app_name}/logs:/var/logs \
--d ${group_name}/${app_name}:${app_version}
+  --link mysql:db \
+  -e 'spring.profiles.active'=${profile_active} \
+  -e TZ="Asia/Shanghai" \
+  -v /etc/localtime:/etc/localtime \
+  -v /mydata/app/${app_name}/logs:/var/logs \
+  -d ${group_name}/${app_name}:${app_version}
 echo '----start container----'
