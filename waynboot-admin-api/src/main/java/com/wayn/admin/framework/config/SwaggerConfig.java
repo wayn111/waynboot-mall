1ca6889225eb7e9d6f313954b270a5f7dd313421
wayn <1669738430@qq.com>
2022-02-08 23:10:50 +0800

    feat(商城): 添加knife4j

--- waynboot-admin-api/src/main/java/com/wayn/admin/framework/config/SwaggerConfig.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/framework/config/SwaggerConfig.java
@@ -11,8 +11,10 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     /**
