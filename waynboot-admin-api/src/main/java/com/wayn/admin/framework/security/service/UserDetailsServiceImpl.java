bb62fa98377eba72e3f27f28cd8c9dc6b7621498
wayn <1669738430@qq.com>
2022-02-13 12:22:04 +0800

    feat(商城): 代码优化

--- waynboot-admin-api/src/main/java/com/wayn/admin/framework/security/service/UserDetailsServiceImpl.java
+++ waynboot-admin-api/src/main/java/com/wayn/admin/framework/security/service/UserDetailsServiceImpl.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -27,9 +26,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private IDeptService iDeptService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private SysPermissionService permissionService;
