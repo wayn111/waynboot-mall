81e991323f18f34866806a5831644205e566993d
wayn <1669738430@qq.com>
2022-11-27 21:59:07 +0800

    feat(商城): 使用MDC实现接口请求调用追踪，以及用户ID记录

---
+++ waynboot-common/src/main/java/com/wayn/common/task/ThreadPoolExecutorMdcWrapper.java
@@ -0,0 +1,30 @@
+package com.wayn.common.task;
+
+
+import com.wayn.common.util.ThreadMdcUtil;
+import org.slf4j.MDC;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+/**
+ * 解决子线程丢失reqest_id问题
+ */
+public class ThreadPoolExecutorMdcWrapper extends ThreadPoolTaskExecutor {
+
+    @Override
+    public void execute(Runnable task) {
+        super.execute(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()));
+    }
+
+    @Override
+    public <T> Future<T> submit(Callable<T> task) {
+        return super.submit(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()));
+    }
+
+    @Override
+    public Future<?> submit(Runnable task) {
+        return super.submit(ThreadMdcUtil.wrap(task, MDC.getCopyOfContextMap()));
+    }
+}
