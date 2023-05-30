2c3b1e5de9709ef588dcc9a02a0db1b97cfac57b
wayn <1669738430@qq.com>
2023-05-30 21:59:41 +0800

    feat(商城): es添加密码

--- waynboot-data/waynboot-data-elastic/src/main/java/com/wayn/data/elastic/config/ElasticClientConfig.java
+++ waynboot-data/waynboot-data-elastic/src/main/java/com/wayn/data/elastic/config/ElasticClientConfig.java
@@ -1,6 +1,10 @@
 package com.wayn.data.elastic.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -12,7 +16,12 @@ import org.springframework.context.annotation.Configuration;
 public class ElasticClientConfig {
     @Bean
     public RestClientBuilder restClientBuilder(ElasticConfig config) {
-        return RestClient.builder(new HttpHost(config.getHost(), config.getPort(), config.getScheme()));
+        RestClientBuilder builder = RestClient.builder(new HttpHost(config.getHost(), config.getPort(), config.getScheme()));
+        // 开始设置用户名和密码
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(config.getUsername(), config.getPassword()));
+        builder.setHttpClientConfigCallback(f -> f.setDefaultCredentialsProvider(credentialsProvider));
+        return builder;
     }
 
     @Bean
--- waynboot-data/waynboot-data-elastic/src/main/java/com/wayn/data/elastic/config/ElasticConfig.java
+++ waynboot-data/waynboot-data-elastic/src/main/java/com/wayn/data/elastic/config/ElasticConfig.java
@@ -11,46 +11,8 @@ public class ElasticConfig {
     public String host;
     public int port;
     public String scheme;
+    public String username;
+    public String password;
     public int shards;
     public int replicas;
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public String getScheme() {
-        return scheme;
-    }
-
-    public void setScheme(String scheme) {
-        this.scheme = scheme;
-    }
-
-    public int getShards() {
-        return shards;
-    }
-
-    public void setShards(int shards) {
-        this.shards = shards;
-    }
-
-    public int getReplicas() {
-        return replicas;
-    }
-
-    public void setReplicas(int replicas) {
-        this.replicas = replicas;
-    }
 }
